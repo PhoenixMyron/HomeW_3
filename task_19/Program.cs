ae28b77bd15084aadb577e403cb0715c0f4143a6
PhoenixMyron <phoenixmyron@gmail.com>
2022-11-28 17:36:25 +0600

    added task 19

---
+++ task_19/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+*/ 
+
+Console.Clear();
+Console.Write("Ведите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = n;
+int result = 0;
+ 
+ while (m > 0) 
+{
+     result *= 10;
+     result += m % 10;
+     m /= 10;
+}
+   if ( n == result )
+     {
+        Console.Write("Да");
+     }
+     else 
+     Console.Write("Нет");
+
